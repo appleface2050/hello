c827d06314e792bd7ae8c9a080cf30bc7c56ea0a
andy-bst <andy@bluestacks.com>
2018-01-24 15:05:34 +0800

    aaa

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/appleface2050/stringutil"
+)
+
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("hello, world"))
 }
